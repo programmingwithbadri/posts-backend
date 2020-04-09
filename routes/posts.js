e5fa22d4f56200564fbeb583431fc822b8d05a10
Badri <badri.py@outlook.com>
2020-04-09 21:19:38 +0530

    save the image using multer package

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const multer = require("multer");
+
 const Post = require("../models/post");
 
 const router = express.Router();
 
-router.post("", (req, res) => {
+const IMG_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpg",
+  "image/jpg": "jpg",
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const ext = IMG_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if (ext) {
+      error = null;
+    }
+    cb(error, "images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLocaleLowerCase().split(" ").join("-");
+    const ext = IMG_TYPE_MAP[file.mimetype];
+    cb(null, name + "-" + Date.now() + "." + ext);
+  },
+});
+
+router.post("", multer(storage).single("image"), (req, res) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
