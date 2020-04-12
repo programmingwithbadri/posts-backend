bd6cf2cbc01172ddd35f9ecfb2bfe7c3ab6d218e
Badri <badri.py@outlook.com>
2020-04-12 16:45:37 +0530

    Using node env variables

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path")
+const path = require("path");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -9,13 +9,17 @@ const postsRoutes = require("./routes/posts");
 const userRoutes = require("./routes/user");
 
 mongoose
-  .connect("mongodb://badri:12td0426@ds239967.mlab.com:39967/heroku_fb2tpnzs")
+  .connect(
+    "mongodb://badri:" +
+      process.env.MONGO_SECRET +
+      "@ds239967.mlab.com:39967/heroku_fb2tpnzs"
+  )
   .then(() => console.log("Connected to MongoDb.."))
   .catch(() => console.log("Connection to MongoDb failed.."));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("images")))
+app.use("/images", express.static(path.join("images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
